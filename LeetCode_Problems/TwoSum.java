6ce8fd34dce7485f0910fad45952cac9dfce4cb3
Bhaskar The Titan <bhaskarjborah33@gmail.com>
2025-04-02 10:32:15 +0530

    Corrected (1) Two sum problem added

--- LeetCode_Problems/TwoSum.java
+++ LeetCode_Problems/TwoSum.java
@@ -7,43 +7,21 @@ public class TwoSum{
 		System.out.println(Arrays.toString(twoSum(nums,target)));
 	}
 	public static int[] twoSum(int [] nums, int target){
-		Arrays.sort(nums); //Sorted the Arrau first;
-		
-		int [] res = new int[2]; //Made an array of size two, because they said only 1 pair exist
+		 //Can't sort the numbers because i need thier indexes.
 
-		int i = 0, j = nums.length-1; //Assigned variables
-		
-		while(i<j){
-			
-			int sum = nums[i]+nums[j]; //Sum Calculation
+		 for(int i = 0; i<nums.length-1; i++){ // start from i at first and j at second
+			 
+			 if(i>0 && nums[i-1]==nums[i])continue; //if we arrive at the second number or the 1st index we check if it equal to the other,if yes we skip
+			 
+			 for(int j = i+1; j<nums.length; j++){ //j starts immediate right of i, that is one step ahead
+				 
+				 int sum = nums[i]+nums[j]; // we store sum in a variable sum
 
-			if(sum==target){ 
-                               /*
-				* We check if the sum == target, if yes we store the indexes, then we check if there exists any duplicate next to us
-				* If yes we go to that index, we do this until we know that i+1 is different
-				* Similarly we do it for j index to, here we just check equal elements before them that is at (j-1)th position
-				* after we have found that i+1 is different and j-1 is different, we do a final increment and decrement of i and j to reach the next and 
-				* previos position
-				* After that loop continues
-				* */
-
-
-				res[0] = i;
-				res[1] = j;
-
-				while(i>j && nums[i]==nums[i+1])i++;
-				while(i>j && nums[j]==nums[j-1])j--;
-
-				i++;
-				j--;
-			}else if(sum>target){ //if sum>target that means we need to look for smaller element, yo j, decrease yourself
-				j--;
-			}else{ // we need to look for greater elements here, yo i, increase yourself
-				i++;
-			}
-                          
-
-		}
-                 return res; //Finally we return the result
+				 if(sum==target){
+					 return new int[] {i,j}; // if we encounter target... Our job is done, return and exit
+				 }
+			 }
+		 }
+                 return new int [] {-1,-1}; //Atlast i will have to return something.. because of the return type
 	}
 }
