644a6db4c20a5c53dc534a0a19c9329f407795ec
Bhaskar The Titan <bhaskarjborah33@gmail.com>
2025-04-02 10:38:16 +0530

    Corrected (2) Two sum problem added

--- LeetCode_Problems/TwoSum.java
+++ LeetCode_Problems/TwoSum.java
@@ -11,8 +11,6 @@ public class TwoSum{
 
 		 for(int i = 0; i<nums.length-1; i++){ // start from i at first and j at second
 			 
-			 if(i>0 && nums[i-1]==nums[i])continue; //if we arrive at the second number or the 1st index we check if it equal to the other,if yes we skip
-			 
 			 for(int j = i+1; j<nums.length; j++){ //j starts immediate right of i, that is one step ahead
 				 
 				 int sum = nums[i]+nums[j]; // we store sum in a variable sum
