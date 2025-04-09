cc098cc7d9d72965f6ceea87e48024a5364ec981
Bhaskar The Titan <bhaskarjborah33@gmail.com>
2025-04-09 09:57:42 +0530

    Corrected (2) Optimized

--- LeetCode_Problems/leetcode73/Optimized/SetMatrixZeroes.java
+++ LeetCode_Problems/leetcode73/Optimized/SetMatrixZeroes.java
@@ -12,44 +12,49 @@ public class SetMatrixZeroes{
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        /*
-         * Intuition: we make used to store the coordinates in a separate data structure. Here we are going to use the top row and col to store them
-         * But there are few key considerations: 1. what if the elements in the top row and cols are itself zero. For that we are going to use a boolean flag
-         * Later we are going to update these values
-         * */
+      
+        boolean firstColHasZero = false;
+        boolean firstRowHasZero = false;
 
-        //We are going to iterate over every element excluding the first column.
-        boolean isFirstColZero = false;
-        boolean isFirstRowZero = false;
-        for(int i = 0; i<rows; i++){
-            if(matrix[i][0]==0) isFirstColZero = true; //if any element in the leftmost col is 0 our flag tracks it
+        for(int j = 0; j<cols; j++){
+            if(matrix[0][j] == 0){
+                firstRowHasZero = true; //Check the topmost one row
+                break;
+            }
             
-            for(int j = 1; j<cols; j++){
-            if(matrix[0][j]==0) isFirstRowZero = true;    
+        }
 
-                if(matrix[i][j] == 0){ // if we find the current element to be 0, we update its location in the top row and the leftmost col
-                   matrix[0][j] = 0; //marking at the top most row at jth index
-                   matrix[i][0] = 0; //marking at the leftmost col at ith index
+        for(int i = 0; i<rows; i++){
+            if(matrix[i][0] == 0){
+                firstColHasZero = true; //Check if the leftMost col has 0
+                break;
+            }
+        
+        }
+
+       
+        
+        for(int i = 1; i<rows; i++){
+            for(int j = 1; j<cols; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-
-        //In the second step we are going to iterate over each element excluding the topmost row and the leftmost column.
-        //And we are going to check the stored results from the topmost and the leftmost col and then update our values to 0
+        
+       
 
         for(int i = 1; i<rows; i++){
             for(int j = 1; j<cols; j++){
-                if(matrix[0][j] == 0 || matrix[i][0]==0){ //if any one, either the top or leftmost contains zero at the current ith and jth index, make it 0;
+                if(matrix[0][j] == 0 || matrix[i][0]==0){ 
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        /*
-         * If isFirstColZero and isFirstRowZero is true that means we encountered 0 that is in either leftMost element or topmost element
-         * There for we manually make the corresponding row and col zeros
-         * */
-        if(isFirstColZero){ 
+    
+        if(firstColHasZero){ 
             for(int i = 0; i<rows; i++){
                 matrix[i][0]=0;
             }
@@ -58,7 +63,7 @@ public class SetMatrixZeroes{
         
 
         
-        if(isFirstRowZero){
+        if(firstRowHasZero){
            for(int j = 0; j<cols; j++){
                matrix[0][j] = 0;
            }
